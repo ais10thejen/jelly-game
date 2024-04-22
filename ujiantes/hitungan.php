b940a27ca5223cafb07fe6f12982bc3969de38ad
ais10thejen <114201706+ais10thejen@users.noreply.github.com>
2024-04-22 19:33:21 +0800

    Add files via upload

---
+++ ujiantes/hitungan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lsp tes</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="berhasil.php">
+        <h1>Perhitungan Online</h1>
+        <input type="number" name="angka1">
+        <select name="operator">
+            <option>+</option>
+            <option>-</option>
+        </select>
+        <input type="number" name="angka2">
+        <button type="submit">Hitung</button>
+    </form>
+</body>
+</html>
