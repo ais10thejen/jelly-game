90f871aa90137cd38c6dda2e37dc8296c544303b
ais10thejen <114201706+ais10thejen@users.noreply.github.com>
2024-04-23 21:48:41 +0800

    Add files via upload

---
+++ apanih/calculator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hitungan</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="hasil.php">
+        <h1>Kalkulator Online</h1>
+        <input type="number" name="angka1">
+        <select name="operator">
+            <option>+</option>
+            <option>-</option>
+        </select>
+        <input type="number" name="angka2">
+        <button type="submit">Hitung</button>
+    </form>
+</body>
+</html>
